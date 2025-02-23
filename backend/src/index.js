e935de409e2b3bca8f2361ffc013e52c87d28c28
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2025-02-23 17:00:14 +0700

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {//testing code:), ignore
+  console.log("ok - temp testing code");
+  res.json({ message: "This is working" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+  connectDB();
+});
