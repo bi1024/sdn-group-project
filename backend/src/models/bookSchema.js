a67083ceaa76543e9740b435af7d3e4f5c52d320
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2025-02-26 14:33:36 +0700

    feat: added bookSchema

---
+++ backend/src/models/bookSchema.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    image: {
+      type: String, // URLs to images
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    categories: [
+      {
+        type: String,
+      },
+    ],
+
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 1,
+    },
+  },
+  { timestamps: true }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+export default Book;
